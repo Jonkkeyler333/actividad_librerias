09d62184f51d808a130da6c683f0c06b1799f09f
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-05-01 17:05:37 -0500

    se crearon los 3 arrays de datos

--- ejercicio2.py
+++ ejercicio2.py
@@ -6,6 +6,7 @@ codigos=np.random.randint(100000, 999999, size=6500)  ##arreglo de los codigos d
 nombres=np.empty(6500, dtype='U50')  ##el argumento 'U50' U de unicode y 50 el tamaño de las cadenas
 for k in range(0,6500):
     nombres[k]=fk.name()
+promedios=np.round(np.random.uniform(0.0,5.0,size=6500))
 
 
 
