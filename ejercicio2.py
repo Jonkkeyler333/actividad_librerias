d730b6dc0a11d723a62138fbd2ad9ae0fbd16d03
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2023-05-01 20:37:45 -0500

    version 1.0F

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,11 +1,28 @@
 from faker import Faker
 import numpy as np
 
-def buscardor_index(A:np.ndarray,B:np.ndarray,promedio:float,programa:int)->list:
+def buscardor_index(A:np.ndarray,B:np.ndarray,C:np.ndarray,promedio:float,programa:int,fecha:int)->list:
+    """Esta función busca los indices de los estudiantes que cumplen con las condiciones dadas
+
+    :param A: Array de informacion del est 
+    :type A: np.ndarray
+    :param B: Array de información del est 
+    :type B: np.ndarray
+    :param promedio: promedio que debe cumplir el estudiante buscado
+    :type promedio: float
+    :param programa: número del programa a buscar (si es 0 , son todos los programas)
+    :type programa: int
+    :return: la lista con los estudiantes(indices) que cumplen las condiciones
+    :rtype: list
+    """
     index=[]
-    if programa == None or programa==0:
+    if programa==0 and fecha==0:
         for j in range(0,6500):
             if A[j]>promedio:
+                index.append(j)
+    elif fecha>0 and programa==0:
+        for j in range(0,6500):
+            if A[j]<=promedio and B[j]==programa and C[j]<fecha:
                 index.append(j)
     else:
         for j in range(0,6500):
@@ -19,8 +36,9 @@ nombres=np.empty(6500, dtype='U50')  ##el argumento 'U50' U de unicode y 50 el t
 for k in range(0,6500):
     nombres[k]=fk.name()
 programas=np.random.randint(1,80,size=6500)
+fecha_ingreso=np.random.randint(1970,2023,size=6500)
 promedios=np.round(np.random.uniform(0.0,5.0,size=6500),decimals=1)
-estudiantes=np.array([codigos,nombres,promedios,programas]) ##Creación de matriz con los datos completos
+estudiantes=np.array([codigos,nombres,promedios,programas,fecha_ingreso]) ##Creación de matriz con los datos completos
 
 if __name__=='__main__':
     print('Bienvedio al sistema')
@@ -29,12 +47,24 @@ if __name__=='__main__':
     des=int(input())
     if des==1:
         prog=int(input('Ingrese el programa (número del 1 al 80):   '))
-        index=buscardor_index(promedios,programas,4.0,prog)
-        print(f'\n Promedios destacados del programa {prog}')
+        index=buscardor_index(promedios,programas,fecha_ingreso,4.0,prog,0)
+        print(f'\n Promedios destacados del programa {prog} : ')
         for ind in index:
-            print(estudiantes[0][ind],estudiantes[1][ind],estudiantes[2][ind],estudiantes[3][ind])
+            print(estudiantes[0][ind],'|',estudiantes[1][ind],'|',estudiantes[2][ind],'|',estudiantes[3][ind],'|',estudiantes[4][ind])
+        print(f'En total fueron {len(index)} estudiantes destacados')
     else:
-        index=buscardor_index(promedios,programas,4.0,0)
+        index=buscardor_index(promedios,programas,fecha_ingreso,4.0,0,0)
         for ind in index:
-            print(estudiantes[0][ind],estudiantes[1][ind],estudiantes[2][ind],estudiantes[3][ind])
+            print(estudiantes[0][ind],'|',estudiantes[1][ind],'|',estudiantes[2][ind],'|',estudiantes[3][ind],'|',estudiantes[4][ind])
         print('Promedios destacados generales')
+        print(f'En total fueron {len(index)} estudiantes destacados')
+
+    inq=[]
+    print('\n\n Ahora se mostraran estudiantes en Condicional con ingreso anterior a 1990')
+    inq=buscardor_index(promedios,programas,fecha_ingreso,3.2,0,1990)
+    if len(inq)==0:
+        print('No se encontro ningún estudiante')
+    else:
+        for ind2 in inq:
+            print(estudiantes[0][ind],'|',estudiantes[1][ind],'|',estudiantes[2][ind],'|',estudiantes[3][ind],'|',estudiantes[4][ind])
+    
